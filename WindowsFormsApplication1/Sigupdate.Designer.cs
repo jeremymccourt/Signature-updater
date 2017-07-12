60093f13d32e9e23834817896dec4b71aba3141e
jeremy.mccourt <jeremy.mccourt@USVMA00027JMCCO>
2017-07-11 20:59:31 -0600

    Add project files.

---
+++ WindowsFormsApplication1/Sigupdate.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Sigupdate
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Sigupdate));
+            this.textBox_FN = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox_JT = new System.Windows.Forms.TextBox();
+            this.textBox_WP = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox_MP = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button_reset = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox_status = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.textBox_presig = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox_FN
+            // 
+            this.textBox_FN.Location = new System.Drawing.Point(103, 196);
+            this.textBox_FN.Name = "textBox_FN";
+            this.textBox_FN.Size = new System.Drawing.Size(212, 20);
+            this.textBox_FN.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 199);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Full Name";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(50, 241);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Job Title";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // textBox_JT
+            // 
+            this.textBox_JT.Location = new System.Drawing.Point(103, 238);
+            this.textBox_JT.Name = "textBox_JT";
+            this.textBox_JT.Size = new System.Drawing.Size(212, 20);
+            this.textBox_JT.TabIndex = 5;
+            // 
+            // textBox_WP
+            // 
+            this.textBox_WP.Location = new System.Drawing.Point(103, 279);
+            this.textBox_WP.Name = "textBox_WP";
+            this.textBox_WP.Size = new System.Drawing.Size(212, 20);
+            this.textBox_WP.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(30, 282);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Work Phone";
+            // 
+            // textBox_MP
+            // 
+            this.textBox_MP.Location = new System.Drawing.Point(103, 320);
+            this.textBox_MP.Name = "textBox_MP";
+            this.textBox_MP.Size = new System.Drawing.Size(212, 20);
+            this.textBox_MP.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(25, 323);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(72, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Mobile Phone";
+            // 
+            // button_reset
+            // 
+            this.button_reset.Location = new System.Drawing.Point(103, 375);
+            this.button_reset.Name = "button_reset";
+            this.button_reset.Size = new System.Drawing.Size(75, 23);
+            this.button_reset.TabIndex = 11;
+            this.button_reset.Text = "Reset";
+            this.button_reset.UseVisualStyleBackColor = true;
+            this.button_reset.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(236, 375);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Submit";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Lucida Bright", 16F);
+            this.label6.Location = new System.Drawing.Point(130, 18);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(241, 24);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "LogRhythm Signature";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Lucida Bright", 16F);
+            this.label7.Location = new System.Drawing.Point(130, 42);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(167, 24);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Updater V1.0.2";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // textBox_status
+            // 
+            this.textBox_status.Enabled = false;
+            this.textBox_status.Location = new System.Drawing.Point(53, 422);
+            this.textBox_status.Name = "textBox_status";
+            this.textBox_status.ReadOnly = true;
+            this.textBox_status.Size = new System.Drawing.Size(308, 20);
+            this.textBox_status.TabIndex = 14;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(2, 425);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(37, 13);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Status";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, -1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(112, 110);
+            this.pictureBox1.TabIndex = 16;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(7, 159);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(89, 13);
+            this.label10.TabIndex = 19;
+            this.label10.Text = "Pre-signature text";
+            // 
+            // textBox_presig
+            // 
+            this.textBox_presig.Location = new System.Drawing.Point(103, 156);
+            this.textBox_presig.Name = "textBox_presig";
+            this.textBox_presig.Size = new System.Drawing.Size(212, 20);
+            this.textBox_presig.TabIndex = 1;
+            this.textBox_presig.Text = "test";
+            // 
+            // Sigupdate
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(433, 464);
+            this.Controls.Add(this.textBox_presig);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBox_status);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button_reset);
+            this.Controls.Add(this.textBox_MP);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox_WP);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox_JT);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_FN);
+            this.Name = "Sigupdate";
+            this.Text = "Signature Editor";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_FN;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox_JT;
+        private System.Windows.Forms.TextBox textBox_WP;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox_MP;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button_reset;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox_status;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox textBox_presig;
+    }
+}
+
---
+++ WindowsFormsApplication1/Sigupdate.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+/* 
+ This is a development program to address the need to get a new LogRhythm signature to all the logrhythm users. 
+ This was created to make it easier than editing an HTML file directly for non-power users. 
+ Jeremy McCourt
+ Enterprise Systems Operations Manager / Global IT
+ jeremy.mccourt@logrhythm.com
+ 2017-07-08
+
+
+     This is a in-progress toolset, and may be developed into more. 
+     Currently this just takes a .raw file (which is actually just an HTML file, that has {FIRSTNAME}, {LASTNAME} etc, written into the html file so that it can easily be replaced. 
+     In the future it would be good to have a choice of different signatures, and figure out which one is current, as have a configuration file that can be edited on deployment, instead of having
+     to recompile. 
+
+     FYI, this is my hello world project for C#. 
+     I also need to add try / catch / throw for exception handling. 
+
+     This is intended to be paired with an MSI to add the files in the correct location, - Also a good reason for a config file. 
+     the MSI will add a link to the start menu, and after some debate on how to deploy, I have written this to be installed in Program files, so LANDESK can deploy as a user with Admin rights. 
+     Also, this can be signed by going to usbo1build05  jenkins / msi_sign 
+    
+ */
+
+namespace WindowsFormsApplication1
+{
+    public partial class Sigupdate : Form
+    {
+        public Sigupdate()
+        {
+            InitializeComponent();
+        }
+       
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public void Form1_Load(object sender, EventArgs e)
+        {
+            
+
+            
+
+           
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox_presig.Text = "";
+            textBox_FN.Text = "";
+           // textBox_LN.Text = "";
+            textBox_JT.Text = "";
+            textBox_WP.Text = "";
+            textBox_MP.Text = "";
+            textBox_postsig.Text = "";
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+
+
+            string fileName = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86) + "\\LR Utilities\\LR Signature Editor\\logrhythm-company-signature-201706.raw";
+            /*if (!File.Exists(fileName))
+            {
+               //fileName = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\Microsoft\\Signatures\\logrhythm-company-signature-201706.raw";
+
+            }
+            */
+
+            string fileNameOut = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\Microsoft\\Signatures\\logrhythm-signature-201706.htm";
+            string fileNameOut_PATH = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\Microsoft\\Signatures";
+            //Read HTML from file
+            if (!Directory.Exists(fileNameOut_PATH))
+            { 
+                Directory.CreateDirectory(fileNameOut_PATH);
+            }
+
+            //
+            var content = File.ReadAllText(fileName);
+            //Replace all values in the HTML
+            content = content.Replace("{PRESIG}", textBox_presig.Text);
+            content = content.Replace("{FULL_NAME}", textBox_FN.Text);
+            //content = content.Replace("{LAST_NAME}", textBox_LN.Text); // Changed first_name, and last_name to full_name
+            content = content.Replace("{JOB_TITLE}", textBox_JT.Text);
+            if (textBox_WP.Text.Length > 5 )
+            { textBox_WP.Text += " (W)"; }
+            content = content.Replace("{WORK_PHONE}", textBox_WP.Text);
+            if (textBox_MP.Text.Length > 5)
+            { textBox_MP.Text += " (M)"; }
+            content = content.Replace("{MOBILE_PHONE}", textBox_MP.Text);
+
+            content = content.Replace("{POSTSIG}", textBox_postsig.Text);
+
+            //Write new HTML string to file
+            File.WriteAllText(fileNameOut, content);
+
+            //Show it in the default application for handling .html files
+            // Process.Start(fileNameOut);
+            textBox_status.Text = "COMPLETED, please wait while program closes.";
+            textBox_status.Update();
+            System.Threading.Thread.Sleep(3000);
+            Close();
+            
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
